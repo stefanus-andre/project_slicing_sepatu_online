c5291793b4994851e1eaa17f3030d3197a3a3053
stefanus-andre <stefanusandre07@gmail.com>
2025-08-01 14:36:06 +0700

    reparasi responsive di slider

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -7,7 +7,7 @@ import "slick-carousel/slick/slick-theme.css";
 const slides = [
   "https://cdn.pixabay.com/photo/2016/11/19/18/06/feet-1840619_1280.jpg", 
   "https://apparelresources.com/wp-content/uploads/2020/03/sneakers-vd.png", 
-  "https://apparelresources.com/wp-content/uploads/2020/03/sneakers-vd2.png"
+  "https://cdn.runrepeat.com/storage/gallery/product_content/25433/nike-dunk-high-ss-21541680-main.jpg"
 ];
 
 export default function HeroSlider() {
